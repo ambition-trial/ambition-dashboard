3030dc9054988af4ae141f41ffc0db10ee5192d6
erikvw <ew2789@gmail.com>
2018-07-05 13:24:22 +0200

    correct org name

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     author_email='ew2789@gmail.com',
     packages=find_packages(),
     include_package_data=True,
-    url='https://github.com/ambition-trail/ambition-dashboard',
+    url='https://github.com/ambition-trial/ambition-dashboard',
     license='GPL license, see LICENSE',
     description='pivoted reference model for edc modules',
     long_description=README,
