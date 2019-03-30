7267025563e6462273b4f7bb371abd512b633962
erikvw <ew2789@gmail.com>
2019-03-29 19:33:36 -0500

    runtests

--- runtests.py
+++ runtests.py
@@ -52,6 +52,7 @@ DEFAULT_SETTINGS = DefaultTestSettings(
         "edc_pharmacy_dashboard.apps.AppConfig",
         "edc_reference.apps.AppConfig",
         "edc_registration.apps.AppConfig",
+        "edc_review_dashboard.apps.AppConfig",
         "edc_subject_dashboard.apps.AppConfig",
         "edc_timepoint.apps.AppConfig",
         "edc_visit_schedule.apps.AppConfig",
